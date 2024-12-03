4f428f402ce8c09c8a0ddd2b118a3d80989a6082
Fabian Peña <fabiancpl91@gmail.com>
2024-12-03 13:45:08 +0000

    Adding script to fine-tune setfit

---
+++ src/train_setfit.py
@@ -0,0 +1,99 @@
+"""Train SetFit.
+
+This module provides the required logic to fine-tune a Sentence-BERT model with a classification head on top using the SetFit framework.
+"""
+
+from tqdm.auto import tqdm
+
+from datasets import load_dataset
+from transformers import (
+    EarlyStoppingCallback,
+    IntervalStrategy,
+)
+from sentence_transformers import losses
+from sentence_transformers.losses.BatchHardTripletLoss import BatchHardTripletLossDistanceFunction
+from setfit import (
+    SetFitModel,
+    TrainingArguments,
+    Trainer,
+)
+
+
+MODEL_ALIASES = {
+    "sentence-transformers/paraphrase-MiniLM-L3-v2": "pml3",
+    "sentence-transformers/all-mpnet-base-v2": "amb",
+    "sentence-transformers/all-MiniLM-L6-v2": "aml6",
+    "sentence-transformers/paraphrase-albert-small-v2": "pas",
+    "sentence-transformers/all-distilroberta-v1": "adr"
+}
+
+NEW_MODEL_NAME = "{alias}-s{strategy}-bs{batch_size}-e{num_epochs}-i{num_iterations}-hLR"
+
+
+def main():
+    
+    model_name = "sentence-transformers/all-distilroberta-v1"
+    multi_target_strategy = "multi-output"
+    batch_size = 32
+    num_epochs = {"java": 5, "python": 10, "pharo": 10}
+    num_iterations = 60
+    
+    langs = ["java", "python", "pharo"]
+    labels = {
+        "java": ["summary", "Ownership", "Expand", "usage", "Pointer", "deprecation", "rational"],
+        "python": ["Usage", "Parameters", "DevelopmentNotes", "Expand", "Summary"],
+        "pharo": ["Keyimplementationpoints", "Example", "Responsibilities", "Classreferences", "Intent", "Keymessages", "Collaborators"]
+    }  
+
+    new_model_name = NEW_MODEL_NAME.format(
+        alias=MODEL_ALIASES[model_name],
+        strategy="MO" if multi_target_strategy == "multi-output" else "OVR",
+        batch_size=batch_size,
+        num_epochs="-".join([str(v) for v in num_epochs.values()]),
+        num_iterations=num_iterations
+    )
+    print("=====")
+    print("New model:", new_model_name)
+    print("=====")
+
+    dataset = load_dataset("NLBSE/nlbse25-code-comment-classification")
+
+    for lan in langs:
+
+        model = SetFitModel.from_pretrained(model_name, multi_target_strategy=multi_target_strategy, device="cuda")
+
+        args = TrainingArguments(
+            output_dir=f"../results/{new_model_name}/{lan}",
+            num_iterations=num_iterations,
+            num_epochs=num_epochs[lan],
+            body_learning_rate=2e-5,
+            head_learning_rate=2e-5,
+            batch_size=batch_size,
+            seed=42,
+            use_amp=False,
+            warmup_proportion=0.1,
+            distance_metric=BatchHardTripletLossDistanceFunction.cosine_distance,
+            margin=0.25,
+            samples_per_label=2,
+            loss=losses.CosineSimilarityLoss,
+        )
+
+        trainer = Trainer(
+            model=model,
+            args=args,
+            train_dataset=dataset[f"{lan}_train"],
+            metric="accuracy",
+            column_mapping={"combo": "text", "labels": "label"}
+        )
+
+        trainer.train()
+
+        trainer.model.save_pretrained(f"../models/{new_model_name}/{lan}")
+
+    print("=====")
+    print("New model:", new_model_name)
+    print("=====")
+
+
+if __name__ == "__main__":
+    main()
